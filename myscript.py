e8d894ddca3c60adf07a41260a1cf48b25ccf01f
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2023-03-21 00:04:27 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,3 +1,11 @@
 import os
-os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
-os.system('git bisect run python manage.py test')
+
+BAD_HASH            = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+GOOD_HASH           = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+BISECT_START_CMD    = 'git bisect start'
+BISECT_RUN_CMD      = 'git bisect run python manage.py test'
+BISECT_RESET_CMD    = 'git bisect reset'
+
+os.system(f'{BISECT_START_CMD} {BAD_HASH} {GOOD_HASH}')
+os.system(BISECT_RUN_CMD)
+os.system(BISECT_RESET_CMD)
